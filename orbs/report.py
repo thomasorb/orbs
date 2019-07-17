3ddc22f81ac3e7bffaa74dc558a2e8479e7b4d24
Thomas Martin <thomas.martin.1@ulaval.ca>
2019-07-17 08:49:43 -0400

    cleaned report.py for old unused code

--- orbs/report.py
+++ orbs/report.py
@@ -52,8 +52,6 @@ class Graph(object):
             except Exception, e:
                 logging.debug('error in getting xlim, ylim parameters: {}'.format(e))
 
-            
-
         self.params = params
         logging.info('generating graph for {}'.format(self.getp('name')))
         path = indexer.get_path(self.getp('name'))
@@ -182,51 +180,12 @@ class Reporter(object):
                 
         self.pdf.output('{}_{}.report.pdf'.format(self.orbs.options['object_name'], self.orbs.options['filter_name']), 'F')
 
-        #     infiles = list()
-        #     infiles_xml = step.findall('infile')
-        #     for infile_xml in infiles_xml:
-        #         infiles.append(infile_xml.attrib['name'])
-        #     outfiles = list()
-        #     outfiles_xml = step.findall('outfile')
-        #     for outfile_xml in outfiles_xml:
-        #         outfiles.append(outfile_xml.attrib['name'])
-                
-        #     self.steps[step.attrib['name']] = Step(infiles,
-        #                                            None,
-        #                                            outfiles)
-
     def get_temp_folder_path(self):
         return './.report.temp/'
 
     def get_data_path(self, filename):
         return self.get_temp_folder_path() + filename
     
-    def generate_pdf(self):
-
-        pdf = FPDF()
-        
-        pdf.add_page()
-        pdf.set_xy(0, 0)
-        pdf.set_font('arial', 'B', 12)
-        pdf.cell(60)
-        pdf.cell(75, 10, "A Tabular and Graphical Report of Professor Criss's Ratings by Users Charles and Mike", 0, 2, 'C')
-        pdf.cell(90, 10, " ", 0, 2, 'C')
-        pdf.cell(-40)
-        pdf.cell(50, 10, 'Question', 1, 0, 'C')
-        pdf.cell(40, 10, 'Charles', 1, 0, 'C')
-        pdf.cell(40, 10, 'Mike', 1, 2, 'C')
-        pdf.cell(-90)
-        pdf.set_font('arial', '', 12)
-        for i in range(0, len(df)):
-            pdf.cell(50, 10, '%s' % (df['Question'].ix[i]), 1, 0, 'C')
-            pdf.cell(40, 10, '%s' % (str(df.Mike.ix[i])), 1, 0, 'C')
-            pdf.cell(40, 10, '%s' % (str(df.Charles.ix[i])), 1, 2, 'C')
-            pdf.cell(-90)
-        pdf.cell(90, 10, " ", 0, 2, 'C')
-        pdf.cell(-30)
-        pdf.image('barchart.png', x = None, y = None, w = 0, h = 0, type = '', link = '')
-        pdf.output('test.pdf', 'F')
-
     def __del__(self):
         shutil.rmtree(self.get_temp_folder_path(), ignore_errors=True)
 
