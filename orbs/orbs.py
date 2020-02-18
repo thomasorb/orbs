4ce52c47d1dfa885e56e69bfa4a3797461a6d4a5
Thomas Martin <thomas.martin.1@ulaval.ca>
2020-02-18 12:49:28 -0500

    phase binning now uses the config keyword PHASE_BINNING

--- orbs/orbs.py
+++ orbs/orbs.py
@@ -1368,12 +1368,7 @@ class Orbs(Tools):
         
         perf = Performance(cube, "Phase map creation", camera_number,
                            instrument=self.instrument)
-
-        if self.config["INSTRUMENT_NAME"] == 'SITELLE':
-            binning = 6
-        else:
-            binning = 3
-
+        
         high_order_phase_path = self._get_phase_file_path(
             self.options['filter_name'])
         
@@ -1384,7 +1379,7 @@ class Orbs(Tools):
             fit_order = self._get_phase_fit_order()
             
 
-        cube.create_phase_maps(binning, fit_order,
+        cube.create_phase_maps(int(self.config['PHASE_BINNING']), fit_order,
                                high_order_phase_path=high_order_phase_path)
             
         perf_stats = perf.print_stats()
